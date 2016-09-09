52b915b33d2c614dd2d577189433f4bf1e671ffd
meil <ics.sora.meil@gmail.com>
2016-09-09 18:20:53 +0900

    Android: fix LoadingHolder layout

--- Twichirp.Android/Resources/Resource.Designer.cs
+++ Twichirp.Android/Resources/Resource.Designer.cs
@@ -2090,59 +2090,59 @@ namespace Twichirp.Android
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f06007a
-			public const int ActionBar = 2131099770;
-			
-			// aapt resource value: 0x7f06007d
-			public const int Background = 2131099773;
-			
 			// aapt resource value: 0x7f06007b
-			public const int Content = 2131099771;
+			public const int ActionBar = 2131099771;
 			
-			// aapt resource value: 0x7f060076
-			public const int Description = 2131099766;
+			// aapt resource value: 0x7f06007e
+			public const int Background = 2131099774;
+			
+			// aapt resource value: 0x7f06007c
+			public const int Content = 2131099772;
+			
+			// aapt resource value: 0x7f060077
+			public const int Description = 2131099767;
 			
 			// aapt resource value: 0x7f060072
 			public const int DrawerLayout = 2131099762;
 			
-			// aapt resource value: 0x7f060082
-			public const int Drop = 2131099778;
+			// aapt resource value: 0x7f060083
+			public const int Drop = 2131099779;
+			
+			// aapt resource value: 0x7f060076
+			public const int GoToWeb = 2131099766;
+			
+			// aapt resource value: 0x7f06007f
+			public const int Icon = 2131099775;
 			
 			// aapt resource value: 0x7f060075
-			public const int GoToWeb = 2131099765;
+			public const int LoadingText = 2131099765;
 			
-			// aapt resource value: 0x7f06007e
-			public const int Icon = 2131099774;
-			
-			// aapt resource value: 0x7f060074
-			public const int LoadingText = 2131099764;
-			
-			// aapt resource value: 0x7f060077
-			public const int Login = 2131099767;
+			// aapt resource value: 0x7f060078
+			public const int Login = 2131099768;
 			
 			// aapt resource value: 0x7f060095
 			public const int Message = 2131099797;
 			
-			// aapt resource value: 0x7f060080
-			public const int Name = 2131099776;
+			// aapt resource value: 0x7f060081
+			public const int Name = 2131099777;
 			
-			// aapt resource value: 0x7f06007c
-			public const int Navigation = 2131099772;
+			// aapt resource value: 0x7f06007d
+			public const int Navigation = 2131099773;
 			
-			// aapt resource value: 0x7f060079
-			public const int Pin = 2131099769;
+			// aapt resource value: 0x7f06007a
+			public const int Pin = 2131099770;
 			
-			// aapt resource value: 0x7f060094
-			public const int ProgressBar = 2131099796;
+			// aapt resource value: 0x7f060074
+			public const int ProgressBar = 2131099764;
 			
 			// aapt resource value: 0x7f060098
 			public const int RecyclerView = 2131099800;
 			
-			// aapt resource value: 0x7f060081
-			public const int ScreenName = 2131099777;
+			// aapt resource value: 0x7f060082
+			public const int ScreenName = 2131099778;
 			
-			// aapt resource value: 0x7f06007f
-			public const int Subtitle = 2131099775;
+			// aapt resource value: 0x7f060080
+			public const int Subtitle = 2131099776;
 			
 			// aapt resource value: 0x7f060097
 			public const int SwipeRefresh = 2131099799;
@@ -2156,8 +2156,8 @@ namespace Twichirp.Android
 			// aapt resource value: 0x7f060073
 			public const int Web = 2131099763;
 			
-			// aapt resource value: 0x7f060083
-			public const int action0 = 2131099779;
+			// aapt resource value: 0x7f060084
+			public const int action0 = 2131099780;
 			
 			// aapt resource value: 0x7f06005a
 			public const int action_bar = 2131099738;
@@ -2183,8 +2183,8 @@ namespace Twichirp.Android
 			// aapt resource value: 0x7f06005b
 			public const int action_context_bar = 2131099739;
 			
-			// aapt resource value: 0x7f060087
-			public const int action_divider = 2131099783;
+			// aapt resource value: 0x7f060088
+			public const int action_divider = 2131099784;
 			
 			// aapt resource value: 0x7f060003
 			public const int action_menu_divider = 2131099651;
@@ -2219,8 +2219,8 @@ namespace Twichirp.Android
 			// aapt resource value: 0x7f060044
 			public const int buttonPanel = 2131099716;
 			
-			// aapt resource value: 0x7f060084
-			public const int cancel_action = 2131099780;
+			// aapt resource value: 0x7f060085
+			public const int cancel_action = 2131099781;
 			
 			// aapt resource value: 0x7f06002b
 			public const int center = 2131099691;
@@ -2234,8 +2234,8 @@ namespace Twichirp.Android
 			// aapt resource value: 0x7f060052
 			public const int checkbox = 2131099730;
 			
-			// aapt resource value: 0x7f06008a
-			public const int chronometer = 2131099786;
+			// aapt resource value: 0x7f06008b
+			public const int chronometer = 2131099787;
 			
 			// aapt resource value: 0x7f060033
 			public const int clip_horizontal = 2131099699;
@@ -2285,8 +2285,8 @@ namespace Twichirp.Android
 			// aapt resource value: 0x7f06001c
 			public const int end = 2131099676;
 			
-			// aapt resource value: 0x7f06008f
-			public const int end_padder = 2131099791;
+			// aapt resource value: 0x7f060090
+			public const int end_padder = 2131099792;
 			
 			// aapt resource value: 0x7f060023
 			public const int enterAlways = 2131099683;
@@ -2324,8 +2324,8 @@ namespace Twichirp.Android
 			// aapt resource value: 0x7f060042
 			public const int icon = 2131099714;
 			
-			// aapt resource value: 0x7f060090
-			public const int icon_frame = 2131099792;
+			// aapt resource value: 0x7f060091
+			public const int icon_frame = 2131099793;
 			
 			// aapt resource value: 0x7f060020
 			public const int ifRoom = 2131099680;
@@ -2333,8 +2333,8 @@ namespace Twichirp.Android
 			// aapt resource value: 0x7f06003f
 			public const int image = 2131099711;
 			
-			// aapt resource value: 0x7f06008e
-			public const int info = 2131099790;
+			// aapt resource value: 0x7f06008f
+			public const int info = 2131099791;
 			
 			// aapt resource value: 0x7f060000
 			public const int item_touch_helper_previous_elevation = 2131099648;
@@ -2342,26 +2342,26 @@ namespace Twichirp.Android
 			// aapt resource value: 0x7f06002f
 			public const int left = 2131099695;
 			
-			// aapt resource value: 0x7f060088
-			public const int line1 = 2131099784;
+			// aapt resource value: 0x7f060089
+			public const int line1 = 2131099785;
 			
-			// aapt resource value: 0x7f06008c
-			public const int line3 = 2131099788;
+			// aapt resource value: 0x7f06008d
+			public const int line3 = 2131099789;
 			
-			// aapt resource value: 0x7f060092
-			public const int list = 2131099794;
+			// aapt resource value: 0x7f060093
+			public const int list = 2131099795;
 			
 			// aapt resource value: 0x7f06000b
 			public const int listMode = 2131099659;
 			
-			// aapt resource value: 0x7f060091
-			public const int list_container = 2131099793;
+			// aapt resource value: 0x7f060092
+			public const int list_container = 2131099794;
 			
 			// aapt resource value: 0x7f060041
 			public const int list_item = 2131099713;
 			
-			// aapt resource value: 0x7f060086
-			public const int media_actions = 2131099782;
+			// aapt resource value: 0x7f060087
+			public const int media_actions = 2131099783;
 			
 			// aapt resource value: 0x7f06001d
 			public const int middle = 2131099677;
@@ -2495,32 +2495,32 @@ namespace Twichirp.Android
 			// aapt resource value: 0x7f060031
 			public const int start = 2131099697;
 			
-			// aapt resource value: 0x7f060085
-			public const int status_bar_latest_event_content = 2131099781;
+			// aapt resource value: 0x7f060086
+			public const int status_bar_latest_event_content = 2131099782;
 			
 			// aapt resource value: 0x7f060065
 			public const int submit_area = 2131099749;
 			
-			// aapt resource value: 0x7f060093
-			public const int switchWidget = 2131099795;
+			// aapt resource value: 0x7f060094
+			public const int switchWidget = 2131099796;
 			
 			// aapt resource value: 0x7f06000d
 			public const int tabMode = 2131099661;
 			
-			// aapt resource value: 0x7f06008d
-			public const int text = 2131099789;
+			// aapt resource value: 0x7f06008e
+			public const int text = 2131099790;
 			
-			// aapt resource value: 0x7f060078
-			public const int text1 = 2131099768;
+			// aapt resource value: 0x7f060079
+			public const int text1 = 2131099769;
 			
-			// aapt resource value: 0x7f06008b
-			public const int text2 = 2131099787;
+			// aapt resource value: 0x7f06008c
+			public const int text2 = 2131099788;
 			
 			// aapt resource value: 0x7f06004d
 			public const int textSpacerNoButtons = 2131099725;
 			
-			// aapt resource value: 0x7f060089
-			public const int time = 2131099785;
+			// aapt resource value: 0x7f06008a
+			public const int time = 2131099786;
 			
 			// aapt resource value: 0x7f060043
 			public const int title = 2131099715;
